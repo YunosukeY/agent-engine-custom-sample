610686aef39171d038cb4e57bb6fa594bece0c70
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2025-05-25 20:47:34 +0900

    message

--- custom_agent.py
+++ custom_agent.py
@@ -1,3 +1,13 @@
+from typing import TypedDict
+
+
+class Message(TypedDict):
+    id: str
+    author: str
+    content: str
+    timestamp: float
+
+
 class CustomAgent:
     def set_up(self):
         from google import genai
@@ -36,25 +46,66 @@ class CustomAgent:
             session_id=session_id,
         )
 
-    def generate_content(self, message: str):
+    def send_message(self, user_id: str, session_id: str, message: str):
+        from google.adk.events import Event
+        from google.genai.types import UserContent, ModelContent
+
+        session = self.session_service.get_session(
+            app_name=self.app_name,
+            user_id=user_id,
+            session_id=session_id,
+        )
+
         response = self.client.models.generate_content(
-            model="gemini-2.0-flash-001",
+            model="gemini-2.5-flash-preview-05-20",
             contents=message,
         )
+
+        user_event = Event(
+            invocation_id=response.response_id,
+            author="user",
+            content=UserContent(message),
+        )
+        self.session_service.append_event(
+            session=session,
+            event=user_event,
+        )
+
+        agent_event = Event(
+            invocation_id=response.response_id,
+            author="agent",
+            content=ModelContent(response.text),
+        )
+        self.session_service.append_event(
+            session=session,
+            event=agent_event,
+        )
+
         return response.text
+
+    def list_messages(self, user_id: str, session_id: str):
+        response = self.session_service.list_events(
+            app_name=self.app_name,
+            user_id=user_id,
+            session_id=session_id,
+        )
+        return [
+            Message(
+                id=event.id,
+                author=event.author,
+                content=event.content.parts[0].text,
+                timestamp=event.timestamp,
+            )
+            for event in response.events
+        ]
 
     def register_operations(self):
         return {
             "": [
-                "generate_content",
                 "create_session",
                 "list_sessions",
                 "delete_session",
+                "send_message",
+                "list_messages",
             ],
         }
-
-
-if __name__ == "__main__":
-    agent = CustomAgent()
-    agent.set_up()
-    print(agent.list_sessions(user_id="test"))
--- get.py
+++ get.py
@@ -2,4 +2,9 @@ from vertexai import agent_engines
 import os
 
 agent_engine = agent_engines.get(os.environ["RESOURCE_ID"])
-print(agent_engine.list_sessions(user_id="test"))
+print(
+    agent_engine.list_messages(
+        user_id="test",
+        session_id="4465438327549460480",
+    )
+)
