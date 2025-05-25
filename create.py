bb9232388fc746d325be56462aa3ab18094e776b
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2025-05-26 07:38:59 +0900

    simplify

--- create.py
+++ create.py
@@ -1,17 +1,3 @@
 from vertexai import agent_engines
-from custom_agent import CustomAgent
-import vertexai
-import os
 
-vertexai.init(
-    project=os.environ["PROJECT_ID"],
-    location=os.environ["LOCATION"],
-    staging_bucket=f"gs://{os.environ['BUCKET_NAME']}",
-)
-
-agent_engine = agent_engines.create(
-    agent_engine=CustomAgent(),
-    display_name="Custom agent sample",
-    requirements=["google-cloud-aiplatform[agent-engines]>=1.94.0"],
-    extra_packages=["custom_agent.py"],
-)
+agent_engines.create()
