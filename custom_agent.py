6ae70ea02603db1d9fb02dca9fa1508b0c0644e6
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2025-05-25 21:26:46 +0900

    stream

--- custom_agent.py
+++ custom_agent.py
@@ -83,6 +83,45 @@ class CustomAgent:
 
         return response.text
 
+    def send_message_stream(self, user_id: str, session_id: str, message: str):
+        from google.adk.events import Event
+        from google.genai.types import UserContent, ModelContent
+
+        session = self.session_service.get_session(
+            app_name=self.app_name,
+            user_id=user_id,
+            session_id=session_id,
+        )
+
+        response_text = ""
+        for chunk in self.client.models.generate_content_stream(
+            model="gemini-2.5-flash-preview-05-20",
+            contents=message,
+        ):
+            response_id = chunk.response_id
+            response_text += chunk.text
+            yield chunk.text
+
+        user_event = Event(
+            invocation_id=response_id,
+            author="user",
+            content=UserContent(message),
+        )
+        self.session_service.append_event(
+            session=session,
+            event=user_event,
+        )
+
+        agent_event = Event(
+            invocation_id=response_id,
+            author="agent",
+            content=ModelContent(response_text),
+        )
+        self.session_service.append_event(
+            session=session,
+            event=agent_event,
+        )
+
     def list_messages(self, user_id: str, session_id: str):
         response = self.session_service.list_events(
             app_name=self.app_name,
@@ -108,4 +147,5 @@ class CustomAgent:
                 "send_message",
                 "list_messages",
             ],
+            "stream": ["send_message_stream"],
         }
--- get.py
+++ get.py
@@ -2,9 +2,9 @@ from vertexai import agent_engines
 import os
 
 agent_engine = agent_engines.get(os.environ["RESOURCE_ID"])
-print(
-    agent_engine.list_messages(
-        user_id="test",
-        session_id="4465438327549460480",
-    )
-)
+for chunk in agent_engine.send_message_stream(
+    user_id="test",
+    session_id="4465438327549460480",
+    message="Hello, how are you?",
+):
+    print(chunk)
